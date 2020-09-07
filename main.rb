46686cab913eab4a9d5fd6cf99faf8e33b16190a
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-09-06 19:52:17 -0500

    chore: ruby integration example

---
+++ main.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'fileutils'
+require 'pdfdetach'
+
+dest_path = "#{__dir__}/tmp"
+src_file = "#{__dir__}/example_041.pdf"
+
+FileUtils.mkdir_p(dest_path) unless File.directory?(dest_path)
+
+pdf = PDFDetach.new(src_file)
+
+puts pdf.list
+puts pdf.saveall({ :output => dest_path })
